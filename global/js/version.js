7ce54fb2c6d641093ff7d7b83e120a0340832ff9
Nikolay Stratiev <n.stratiev@gmail.com>
2025-11-20 01:49:37 +0200

    Offline Banitsa Calculator - TEST

--- global/js/version.js
+++
@@ -1,7 +0,0 @@
-export function printCurrentVersion(versionElem) {
-  printCalculatorVersion(versionElem, '0.9');
-}
-
-function printCalculatorVersion(versionElem, calculatorVersion) {
-  versionElem.textContent = calculatorVersion;
-}
